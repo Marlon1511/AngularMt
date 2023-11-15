0cab9ada78f7d95f08ba49ce1b591c114f1e53fd
Marlon1511 <marlon.giral1511@gmail.com>
2023-11-14 20:01:26 -0500

    Inicio Proyecto

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'proyecto-angularMT';
+  public imagePath = '/assets/mt09.png';
+}
